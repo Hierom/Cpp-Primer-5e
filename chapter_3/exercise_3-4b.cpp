25191ec3ab0dc6eb045d08ea9d8c75f94e1047c5
Hierom <jrong.ece05g@nctu.edu.tw>
2018-04-22 22:49:37 +0800

    Ex 3-4 & 3-5

---
+++ chapter_3/exercise_3-4b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1, str2;
+  while (cin >> str1 >> str2) {
+    if (str1.size() == str2.size())
+      cout << "The two strings have same length." << endl;
+    else
+      cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2);
+  }
+  return 0;
+}
---
+++ chapter_3/exercise_3-5b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+  string str, buffer;
+  while (cin >> buffer) {
+    str += (str.empty() ? "" : " ") + buffer;
+    cout << "The concatenated string is " << str << endl;
+  }
+
+  return 0;
+}
