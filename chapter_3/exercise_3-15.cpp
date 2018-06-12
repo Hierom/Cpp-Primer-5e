9e4a30e90f34456606f96c86b6609274af32a2dd
Hierom <jrong.ece05g@nctu.edu.tw>
2018-06-12 21:38:57 +0800

    3-15

---
+++ chapter_3/exercise_3-15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<string> vecstring;
+  for (string buffer; cin >> buffer; vecint.push_back(buffer))
+    ;
+
+  return 0;
+}
