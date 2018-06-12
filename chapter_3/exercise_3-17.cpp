ab331e6320ec65d1c46d29312b54775758223c67
Hierom <jrong.ece05g@nctu.edu.tw>
2018-06-12 23:13:49 +0800

    3-17

---
+++ chapter_3/exercise_3-17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<string> vecstring;
+  for (string word; cin >> word; vecstring.push_back(word))
+    ;
+  for (auto &str8 : vecstring)
+    for (auto &c : str8) c = toupper(c);
+  for (decltype(vecstring.size()) i = 0; i != vecstring.size(); ++i) {
+    if (i != 0 && i % 8 == 0) cout << endl;
+    cout << vecstring[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
