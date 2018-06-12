66fc0f3f61c486c31eeb82ba7bbdd3886f3862e9
Hierom <jrong.ece05g@nctu.edu.tw>
2018-06-12 23:19:34 +0800

    3-18

---
+++ chapter_3/exercise_3-18.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  // vector<int> ivec;
+  // ivec[0] = 42;
+  vector<int> ivec{10};
+  cout << ivec[0] << endl;
+  return 0;
+}
