7fdc59d38b4d5055131232eaae2df2ed9cbe5fd2
Hierom <jrong.ece05g@nctu.edu.tw>
2018-06-12 23:14:48 +0800

    revise 3-17

--- chapter_3/exercise_3-17.cpp
+++ chapter_3/exercise_3-17.cpp
@@ -8,8 +8,8 @@ int main() {
   vector<string> vecstring;
   for (string word; cin >> word; vecstring.push_back(word))
     ;
-  for (auto &str8 : vecstring)
-    for (auto &c : str8) c = toupper(c);
+  for (auto &str : vecstring)
+    for (auto &c : str) c = toupper(c);
   for (decltype(vecstring.size()) i = 0; i != vecstring.size(); ++i) {
     if (i != 0 && i % 8 == 0) cout << endl;
     cout << vecstring[i] << " ";
